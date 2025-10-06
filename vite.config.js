41d773fac02064fb8b4754fa584542c22b09c89b
Helder Santos <167047701+HelderSantos-15@users.noreply.github.com>
2025-10-06 00:18:16 +0000

    Atualizando alterações do mini-blog

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// Configuração completa para CodeSandbox
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: true,
+    port: 2222,
+    allowedHosts: [".csb.app", ".codesandbox.io"],
+  },
+});
